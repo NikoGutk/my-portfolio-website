249cf3b8affad08994c7ec16d28958abe444695e
NikoGutk <mykolagutnyk0799@gmail.com>
2026-02-03 21:37:43 +0100

    changed toolkit descriptions, added script for hover

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+
+let isMouseOverGrid = false;
+const grid = document.querySelector('.grid');
+
+grid.addEventListener('mouseenter', () => isMouseOverGrid = true);
+grid.addEventListener('mouseleave', () => isMouseOverGrid = false);
+
+function autoHover() {
+
+    if (isMouseOverGrid) {
+       
+        setTimeout(autoHover, 1000);
+        return;
+    }
+
+    const cards = document.querySelectorAll('.grid-card');
+
+    cards.forEach(card => card.classList.remove('active'));
+
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    const randomCard = cards[randomIndex];
+
+    randomCard.classList.add('active');
+
+    setTimeout(() => {
+        randomCard.classList.remove('active');
+
+        setTimeout(autoHover, 4000);
+
+    }, 6000);
+} 
+
+setTimeout(autoHover, 2000);
+
+
